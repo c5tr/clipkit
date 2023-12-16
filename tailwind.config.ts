722851174c6870b3a4ea1badf4f055bfcbd65555
rdcsq <git@rdcsq.me>
2023-12-16 00:09:24 -0600

    add iconify bi icons

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,15 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
+import { iconsPlugin, getIconCollections } from "@egoist/tailwindcss-icons";
 
 const config: Config = {
   content: [
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  plugins: [],
-}
-export default config
+  plugins: [
+    iconsPlugin({
+      collections: getIconCollections(["bi"]),
+    }),
+  ],
+};
+export default config;
