296ad865bb0aa3561db56879d083c910f39659bd
rdcsq <git@rdcsq.me>
2023-12-15 23:58:22 -0600

    create user id index in clips table

--- src/data/db/schema.ts
+++ src/data/db/schema.ts
@@ -24,15 +24,21 @@ export const users = sqliteTable(
   }),
 );
 
-export const clips = sqliteTable("clips", {
-  id: text("id").notNull().primaryKey(),
-  userId: integer("user_id")
-    .references(() => users.id, { onDelete: "cascade" })
-    .notNull(),
-  title: text("title").notNull(),
-  status: text("status").default("uploading").notNull(),
-  videoFormat: text("video_format").notNull(),
-});
+export const clips = sqliteTable(
+  "clips",
+  {
+    id: text("id").notNull().primaryKey(),
+    userId: integer("user_id")
+      .references(() => users.id, { onDelete: "cascade" })
+      .notNull(),
+    title: text("title").notNull(),
+    status: text("status").default("uploading").notNull(),
+    videoFormat: text("video_format").notNull(),
+  },
+  (table) => ({
+    userIdIdx: index("user_id_idx").on(table.userId),
+  }),
+);
 
 export const usersRelations = relations(users, ({ many }) => ({
   clips: many(clips),
