b46a3bc42f5d2e0c51514adb12cfed18cce6b0d4
rdcsq <git@rdcsq.me>
2023-12-17 20:09:32 -0600

    add clip display page

---
+++ src/app/(app)/v/[slug]/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="text-center">
+      <div className="i-bi-camera-video-off text-4xl" />
+      <h2 className="font-bold">Clip not found</h2>
+    </main>
+  );
+}
--- src/app/(app)/v/[slug]/page.tsx
+++ src/app/(app)/v/[slug]/page.tsx
@@ -1,7 +1,42 @@
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
+import { cache } from "react";
 import { ClipsService } from "~/data/clips";
 
-export default async function VideoPlayer({ params }: { params: { slug: string } }) {
-  const clip = await ClipsService.getClipById(params.slug);
-  
-  return <p>{JSON.stringify(params)}</p>;
+const cachedGetClipById = cache(
+  async (id: string) => await ClipsService.getClipById(id),
+);
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { slug: string };
+}): Promise<Metadata> {
+  const clip = await cachedGetClipById(params.slug);
+  if (!clip) notFound();
+
+  return {
+    title: clip.title,
+  };
+}
+
+export default async function VideoPlayer({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const clip = await cachedGetClipById(params.slug);
+  if (!clip) notFound();
+
+  return (
+    <>
+      <video
+        src={`${process.env.S3_PUBLIC_URL}/${params.slug}.${clip.videoFormat}`}
+        poster={`${process.env.S3_PUBLIC_URL}/${params.slug}.webp`}
+        className="aspect-video w-full"
+        controls
+      />
+      <h2 className="mt-4 font-bold">{clip.title}</h2>
+    </>
+  );
 }
